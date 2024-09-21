b92648c5cbd622fc6473eb401b64856bd78b5198
Reichen <119686482+Breichen1@users.noreply.github.com>
2024-09-21 15:47:59 -0500

    Added an auto shoot

---
+++ src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants.LauncherConstants;
+import frc.robot.subsystems.CANDrivetrain;
+import frc.robot.subsystems.CANLauncher;
+
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  public static Command exampleAuto(CANDrivetrain drivetrain, CANLauncher m_launcher) {
+    /**
+     * RunCommand is a helper class that creates a command from a single method, in this case we
+     * pass it the arcadeDrive method to drive straight back at half power. We modify that command
+     * with the .withTimeout(1) decorator to timeout after 1 second, and use the .andThen decorator
+     * to stop the drivetrain after the first command times out
+     */
+       return new PrepareLaunch(m_launcher)
+       .withTimeout(LauncherConstants.kLauncherDelay)
+       .andThen(new LaunchNote(m_launcher))
+       .withTimeout(2)
+    
+
+        .andThen (new RunCommand(() -> drivetrain.arcadeDrive(.5, 0), drivetrain)
+        .withTimeout(2)
+        .andThen(new RunCommand(() -> drivetrain.arcadeDrive(0, 0), drivetrain)));
+
+       
+
+
+    /*  return new RunCommand(() -> drivetrain.arcadeDrive(-.5, 0), drivetrain)
+        .withTimeout(1)
+        .andThen(new RunCommand(() -> drivetrain.arcadeDrive(0, 0), drivetrain)); */
+  }
+
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
